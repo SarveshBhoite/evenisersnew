e305d74631167cd5a4ab2cc54ad84042dec62c3f
SarveshBhoite <rajb81008@gmail.com>
2026-02-01 19:21:56 +0530

    romantic data added

--- seedappend.js
+++ seedappend.js
@@ -9,7 +9,7 @@ const Product = require("./models/Product"); // Ensure this matches your file ca
 const User = require("./models/Users");
 
 // 👇 CHANGE THIS LINE to point to the file you want to seed
-const productData = require("./data/namingceremony"); 
+const productData = require("./data/romantic"); 
 
 dotenv.config();
 
@@ -46,7 +46,7 @@ mongoose.connect(process.env.MONGO_URI)
 const findImageFile = (filename) => {
     if (!filename) return null;
     
-    const basePath = path.join(__dirname, "data", "namingceremony");
+    const basePath = path.join(__dirname, "data", "romantic");
     const originalPath = path.join(basePath, filename);
 
     if (fs.existsSync(originalPath)) return originalPath;
