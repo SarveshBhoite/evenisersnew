82c595cb8fa811fdbf409818eccb3188656f1460
SarveshBhoite <rajb81008@gmail.com>
2026-02-09 17:44:35 +0530

    fg

--- frontend/app/refund-policy/page.tsx
+++ frontend/app/refund-policy/page.tsx
@@ -159,7 +159,6 @@ export default function RefundPage() {
         </div>
       </section>
 
-      <Footer />
     </div>
   );
 }
--- frontend/app/why/page.tsx
+++ frontend/app/why/page.tsx
@@ -126,7 +126,6 @@ export default function WhyUsPage() {
         </div>
       </section>
 
-      <Footer />
     </div>
   );
 }
