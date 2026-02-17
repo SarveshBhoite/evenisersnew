d5064ee50723a6accc1daf9a95f3956333840372
SarveshBhoite <rajb81008@gmail.com>
2026-02-17 17:24:54 +0530

    change

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -164,7 +164,7 @@ export default function ProductPage() {
                     }
                     alt={product.name}
                     fill
-                    className="object-cover"
+                    className="object-contain bg-white"
                     priority
                 />
                 
